715da9bc06a68ae7111567ea0b25fd1aa7701c0d
petersalex27 <petersalex27@outlook.com>
2022-09-20 19:31:47 -0500

    update

--- p1.js
+++ p1.js
@@ -1,78 +1,81 @@
-const Loc = {
-    LEFT : 0,
-    UP : 0,
-    RIGHT : 1,
-    DOWN : 1
+function getColor(red, green, blue) {
+    return "#".concat(red.toString(16).padStart(2, "0"),
+        green.toString(16).padStart(2, "0"),
+        blue.toString(16).padStart(2, "0"));
 }
 
 window.onload = function() {
     // init
     var canvas = document.getElementById("myCanvas");
+    var canvas2 = document.getElementById("myCanvas2");
     var depthSlider = document.getElementById("depth");
+    var spinSlider = document.getElementById("spin");
+    var ampSlider = document.getElementById("amp");
+    var periodSlider = document.getElementById("period");
     var context = canvas.getContext("2d");
+    var context2 = canvas2.getContext("2d");
     let wOut = 400;
     let hOut = wOut;
     let xOut = 0;
     let yOut = xOut;
-    depthSlider.value = 0;
+    depthSlider.value = depthSlider.max;
+    spinSlider.value = spinSlider.min;
+    ampSlider.value = 50;
+    periodSlider.value = 100;
     // init listener
     depthSlider.addEventListener("input", drawShape);
+    spinSlider.addEventListener("input", drawShape);
+    ampSlider.addEventListener("input", drawShape);
+    periodSlider.addEventListener("input", drawShape);
     // initial draw
     drawShape();
 
     // definitions
     function drawShape() {
-        let wInner = 400 + (4 * depthSlider.value);
-        let hInner = 400 + (4 * depthSlider.value);
-        let xInner = -(2 * depthSlider.value);
-        let yInner = -(2 * depthSlider.value);
-        let i = 0, colr = 0x59, colg = 0x6b, colb = 0xdf;
-        for (; i < 400 - wInner; ++i) {
-            draw(xOut + Math.floor(i/2), yOut + Math.floor(i/2), wOut - i, hOut - i,
-                 "#".concat(colr.toString(16).padStart(2, "0"),
-                            colg.toString(16).padStart(2, "0"),
-                            colb.toString(16).padStart(2, "0")));
-            colr = (i % Math.ceil(400 / 0x59)) ? colr : colr - 1;
-            colg = (i % Math.ceil(400 / 0x6b)) ? colg : colg - 1;
-            colb = (i % Math.ceil(400 / 0xdf)) ? colb : colb - 1;
+        context.reset();
+        context2.reset();
+        drawDepth(-4 * depthSlider.value);
+        drawWave();
+
+        function drawDepth(howDeep, red = 0x59, green = 0x6b, blue = 0xdf) {
+            let i = 0;
+            const cRed = red, cGreen = green, cBlue = blue;
+
+            context.beginPath();
+            context.rect(0, 0, 400, 400);
+            context.fillStyle = getColor(red, green, blue);
+            context.fill();
+
+            for ( ; i < howDeep || i === 0; ++i) {
+                let c = getColor(red, green, blue);
+                drawRect(xOut + Math.floor(i/2), yOut + Math.floor(i/2), wOut - i, hOut - i, c, i);
+                red = (i % Math.ceil(400 / cRed)) ? red : red - 1;
+                green = (i % Math.ceil(400 / cGreen)) ? green : green - 1;
+                blue = (i % Math.ceil(400 / cBlue)) ? blue : blue - 1;
+            }
         }
 
-        col = "#".concat((colr + 0x20).toString(16).padStart(2, "0"),
-                         (colg + 0x20).toString(16).padStart(2, "0"),
-                         colb.toString(16).padStart(2, "0"));
-        draw(xInner, yInner, wInner, hInner, col);
+        function drawWave() {
+            context2.beginPath();
+            for (let i = 0; i < 400; ) {
+                context2.ellipse(i, 200, periodSlider.value, ampSlider.value, 0, 0, 180);
+                i += periodSlider.value;
+                context2.ellipse(i, 200, periodSlider.value, ampSlider.value, 0, 0, 180, true);
+                i += periodSlider.value;
+            }
+            context2.stroke();
+        }
 
-
-        function draw(x, y, w, h, col) {
-            //context.reset();
+        function drawRect(x, y, w, h, col, currDepth) {
             context.beginPath();
+            if (spinSlider.value != 0) {
+                context.translate(200, 200);
+                context.rotate((currDepth + spinSlider.value) * Math.PI / 180);
+                context.translate(-200, -200);
+            }
             context.rect(x, y, w, h);
             context.fillStyle = col;
             context.fill();
-        }
-
-        function drawLines(x, baseCol) {
-            // top
-            for (var i = 0; i < x; ++i) {
-                context.beginPath();
-                context.moveTo(0, 0);
-                context.lineTo(i, i);
-            }
-
-            // top right
-            context.moveTo(400, 0);
-            context.lineTo(400 - x, y);
-            // bottom left
-            context.moveTo(0, 400);
-            context.lineTo(x, 400 - y);
-            // bottom right
-            context.moveTo(400, 400);
-            context.lineTo(400 - x, 400 - y);
-            context.stroke();
-        }
-
-        function computeCol(r, g, b, loc) {
-
         }
     }
 }
